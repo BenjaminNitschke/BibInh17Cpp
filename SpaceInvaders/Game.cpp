a284ecece2f4535392a450f8027287d330d63ca5
Benjamin Nitschke <Benjamin@DeltaEngine.net>
2018-10-26 14:16:18 +0200

    space invaders stable with background, ship, input and enemy

--- SpaceInvaders/Game.cpp
+++ SpaceInvaders/Game.cpp
@@ -1,11 +1,23 @@
 #include "stdafx.h"
 #include "Game.h"
 
+bool Game::leftPressed;
+bool Game::rightPressed;
+
+void OnKeyboard(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_LEFT)
+		Game::leftPressed = action != GLFW_RELEASE;
+	if (key == GLFW_KEY_RIGHT)
+		Game::rightPressed = action != GLFW_RELEASE;
+}
+
 Game::Game() 
 {
 	glfwInit();
 	window = glfwCreateWindow(1280, 720, "SpaceInvaders", NULL, NULL);
 	glfwMakeContextCurrent(window);
+	glfwSetKeyCallback(window, OnKeyboard);
 }
 
 Game::~Game() 
--- SpaceInvaders/Sprite.h
+++ SpaceInvaders/Sprite.h
@@ -8,6 +8,11 @@ public:
 	Sprite(std::shared_ptr<Texture> texture, int x, int y)
 		: texture(texture), x(x), y(y) {}
 	void Draw();
+	void Move(int addX, int addY)
+	{
+		x += addX;
+		y += addY;
+	}
 private:
 	std::shared_ptr<Texture> texture;
 	int x;
