02614e5c02bf605dc39df79074339da421d0ade1
MarvinKnoche <marvinknoche@t-online.de>
2018-11-09 14:20:13 +0100

    fps

---
+++ CppGameEngine/Game.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <functional>
+
+class Game
+{
+private:
+	void* window;
+public:
+	Game();
+	~Game();
+	void Run(std::function<void()> renderFunction);
+	void RunTriangle();
+	static bool leftPressed;
+	static bool rightPressed;
+	static bool spacePressed;
+	double time = 0;
+	double lastTime = 0;
+	double timeThisTick = 0.001;
+	float GetTimeDelta() { return static_cast<float>(timeThisTick); }
+};
---
+++ CppGameEngine/Sprite.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <memory>
+#include "Texture.h"
+
+class Sprite
+{
+public:
+	Sprite(std::shared_ptr<Texture> texture, int x, int y)
+		: texture(texture), x(x), y(y) {}
+	void Draw();
+	void Move(int addX, int addY);
+private:
+	std::shared_ptr<Texture> texture;
+	int x;
+	int y;
+};
+
