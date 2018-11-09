97a15936dd56159680cf7893e98e175af3634c7e
Benjamin Nitschke <Benjamin@DeltaEngine.net>
2018-11-09 12:53:49 +0100

    Fps and CppGameEngine projects

---
+++ CppGameEngine/Game.cpp
@@ -0,0 +1,68 @@
+#include "stdafx.h"
+#include "Game.h"
+#include <GLFW/glfw3.h>
+
+bool Game::leftPressed;
+bool Game::rightPressed;
+bool Game::spacePressed;
+
+void OnKeyboard(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_LEFT)
+		Game::leftPressed = action != GLFW_RELEASE;
+	if (key == GLFW_KEY_RIGHT)
+		Game::rightPressed = action != GLFW_RELEASE;
+	if (key == GLFW_KEY_SPACE)
+		Game::spacePressed = action != GLFW_RELEASE;
+	if (key == GLFW_KEY_ESCAPE)
+		glfwSetWindowShouldClose(window, true);
+}
+
+Game::Game() 
+{
+	glfwInit();
+	window = glfwCreateWindow(1280, 720, "SpaceInvaders", NULL, NULL);
+	auto glwWindow = (GLFWwindow*)window;
+	glfwMakeContextCurrent(glwWindow);
+	glfwSetKeyCallback(glwWindow, OnKeyboard);
+}
+
+Game::~Game() 
+{
+	glfwTerminate();
+}
+
+void Game::Run(std::function<void()> renderFunction)
+{
+	int viewportWidth;
+	int viewportHeight;
+	auto glwWindow = (GLFWwindow*)window;
+	while (!glfwWindowShouldClose(glwWindow))
+	{
+		glfwGetWindowSize(glwWindow, &viewportWidth, &viewportHeight);
+		glViewport(0, 0, viewportWidth, viewportHeight);
+		glClearColor(0, 0, 0, 1);
+		glClear(GL_COLOR_BUFFER_BIT);
+		time = glfwGetTime();
+		timeThisTick = time - lastTime;
+		lastTime = time;
+		renderFunction();
+		glfwSwapBuffers(glwWindow);
+		glfwPollEvents();
+	}
+}
+
+void Game::RunTriangle()
+{
+	Run([=]()
+	{
+		glBegin(GL_TRIANGLES);
+		glColor3f(1.f, 0.f, 0.f);
+		glVertex3f(-0.6f, -0.4f, 0.f);
+		glColor3f(0.f, 1.f, 0.f);
+		glVertex3f(0.6f, -0.4f, 0.f);
+		glColor3f(0.f, 0.f, 1.f);
+		glVertex3f(0.f, 0.6f, 0.f);
+		glEnd();
+	});
+}
