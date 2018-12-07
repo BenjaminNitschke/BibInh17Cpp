2ee7ac67a8fbdd5e65ff13ee786a47bf08e39df9
Benjamin Nitschke <Benjamin@DeltaEngine.net>
2018-12-07 12:18:34 +0100

    Added SetupProjection and UpdateCamera

--- Fps/FpsGame.cpp
+++ Fps/FpsGame.cpp
@@ -2,36 +2,42 @@
 #include "FpsGame.h"
 #include "Sprite.h"
 
+void FpsGame::SetupProjection()
+{
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	float zNear = 0.1f;
+	float zFar = 100.0f;
+	GLfloat aspect = viewportWidth / (float)viewportHeight;
+	float fov = 60.0f;
+	float fovH = tan(float(fov * DegreeToRadians)) * zNear;
+	float fovW = fovH * aspect;
+	glFrustum(-fovW, fovW, -fovH, fovH, zNear, zFar);
+}
+
+void FpsGame::UpdateCamera()
+{
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glRotatef(-90, 1, 0, 0);
+	if (leftPressed)
+		rotation -= 0.4f;
+	else if (rightPressed)
+		rotation += 0.4f;
+	glRotatef(rotation, 0, 0, 1);
+	if (spacePressed){
+		auto MovementSpeed = 0.023f;
+		movement.x += -sin(rotation*DegreeToRadians) * MovementSpeed;
+		movement.y += -cos(rotation*DegreeToRadians) * MovementSpeed;
+	}
+	glTranslatef(movement.x, movement.y, -2);
+}
+
 void FpsGame::RunGame()
 {
 	Run([=]()
 	{
-		// camera fun
-		glMatrixMode(GL_PROJECTION);
-		glLoadIdentity();
-		float zNear = 0.1f;
-		float zFar = 100.0f;
-		GLfloat aspect = viewportWidth / (float)viewportHeight;
-		float fov = 60.0f;
-		auto DegreeToRadians = 1.0f / 360.0f * 3.14159f;
-		float fovH = tan(float(fov * DegreeToRadians)) * zNear;
-		float fovW = fovH * aspect;
-		glFrustum(-fovW, fovW, -fovH, fovH, zNear, zFar);
-
-		glMatrixMode(GL_MODELVIEW);
-		glLoadIdentity();
-		glRotatef(-90, 1, 0, 0);
-		if (leftPressed)
-			rotation -= 0.4f;
-		else if (rightPressed)
-			rotation += 0.4f;
-		glRotatef(rotation, 0, 0, 1);
-		if (spacePressed){
-			auto MovementSpeed = 0.023f;
-			movement.x += -sin(rotation*DegreeToRadians) * MovementSpeed;
-			movement.y += -cos(rotation*DegreeToRadians) * MovementSpeed;
-		}
-		glTranslatef(movement.x, movement.y, -2);
+		UpdateCamera();
 
 		glBindTexture(GL_TEXTURE_2D, groundTexture->handle);
 		glEnable(GL_TEXTURE_2D);
--- Fps/FpsGame.h
+++ Fps/FpsGame.h
@@ -9,11 +9,15 @@ class FpsGame : public Game
 	std::shared_ptr<Texture> groundTexture;
 	float rotation = 0;
 	Vector3 movement = Vector3(0, 0, 0);
+	float DegreeToRadians = (1.0f / 180.0f) * 3.14159f;
+	void SetupProjection();
+	void UpdateCamera();
 
 public:
 	FpsGame() : Game("Fps")
 	{
 		groundTexture = std::make_shared<Texture>("Ground.png");
+		SetupProjection();
 	}
 	~FpsGame()
 	{
