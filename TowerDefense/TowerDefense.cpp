bca5aea365ad3653c3de172c839d3537d585341d
MarvinKnoche <38417545+MarvinKnoche@users.noreply.github.com>
2019-01-11 12:53:06 +0100

    TowerDefense

---
+++ TowerDefense/TowerDefense.cpp
@@ -0,0 +1,10 @@
+#include "stdafx.h"
+#include "TowerDefense.h"
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    return 0;
+}
