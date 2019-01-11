58f2a0127568021e6c34ca05da59782555801194
Daniel <38142517+rausc-daniel@users.noreply.github.com>
2019-01-11 14:45:32 +0100

    CanastaProject

---
+++ Canasta/Canasta/CanastaGame.cpp
@@ -0,0 +1,11 @@
+#include "stdafx.h"
+#include "CanastaGame.h"
+
+void CanastaGame::RunGame()
+{
+	Run([=]()
+	{
+		
+	});
+}
+
---
+++ Canasta/Canasta/CanastaGame.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Game.h""
+
+
+class CanastaGame : public Game
+{
+public:
+	CanastaGame() : Game("Canasta")
+	{
+		
+	}
+	~CanastaGame()
+	{
+		
+	}	
+	void RunGame();
+};
