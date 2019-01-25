c6a299332c2735107371ff4fa9d0946e75580ee9
Kevin <38255521+Snowball115@users.noreply.github.com>
2019-01-25 14:57:11 +0100

    commit

--- EndlessRunner/EndlessRunner/EndlessRunnerGame.cpp
+++ EndlessRunner/EndlessRunner/EndlessRunnerGame.cpp
@@ -10,7 +10,7 @@ const float height = 1080;
 EndlessRunnerGame::EndlessRunnerGame(int width, int height, const char* name) : Game(width, height, name)
 {
 	road = std::make_shared<Sprite>(std::make_shared<Texture>("../Pics/road.png"), width / 2, height / 2);
-	playerCar = std::make_shared<Sprite>(std::make_shared<Texture>("../Pics/playerCar.png"), width / 2, height * 0.1f);
+	playerCar = std::make_shared<Sprite>(std::make_shared<Texture>("../Pics/playerCar.png"), width / 2, height * 0.8f);
 }
 
 EndlessRunnerGame::~EndlessRunnerGame()
