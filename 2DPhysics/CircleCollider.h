3031a2acad88ae778a1fe32e52866ab1092de86d
felix13245 <38417627+felix13245@users.noreply.github.com>
2019-02-08 20:54:20 +0100

    Create CircleCollider.h

---
+++ 2DPhysics/CircleCollider.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Collider.h"
+#include <memory>
+class CircleCollider : public Collider
+{
+public:
+	CircleCollider(std::shared_ptr<Vector3>  mycenter)
+	{
+		center = mycenter;
+	}
+
+	~CircleCollider();
+};
