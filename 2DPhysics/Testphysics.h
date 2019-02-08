f1c5f9f50f0c1c28a5d585ed6884456fc4d1d3db
felix13245 <38417627+felix13245@users.noreply.github.com>
2019-02-08 20:54:47 +0100

    Create Testphysics.h

---
+++ 2DPhysics/Testphysics.h
@@ -0,0 +1,14 @@
+#include "Game.h"
+
+#include <vector>
+#include "GameObject.h"
+class Testphysics : public Game
+{
+public:
+	Testphysics();
+	~Testphysics();
+	void RunTestphysics();
+	
+
+	void DrawAll();
+};
